b0546e52c258450294d9ad1e766e235b288988be
Caleb Crosby <caleb@cicsolutions.com>
2014-07-19 13:39:06 -0600

    Figure out Namespace/Class Name
    - still can’t get it to work :(

--- Module.php
+++ Module.php
@@ -1,13 +1,15 @@
 <?php
 
-namespace cics\modules;
+namespace cics\modules\testimonials;
 
-class Testimonial extends \yii\base\Module
+/* use Yii; */
+
+class Module extends \yii\base\Module
 {
 	public function init()
 	{
 	    parent::init();
 	    // initialize the module with the configuration loaded from config.php
-	    \Yii::configure($this, require(__DIR__ . '/config.php'));
+/* 	    \Yii::configure($this, require(__DIR__ . '/config.php')); */
 	}
 }
